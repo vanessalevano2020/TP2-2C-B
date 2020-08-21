dbe110475dae6c5fd3599836786f28f6aa87f60b
pablofernandezhinojosa <pablof@tecnoshare.com>
2020-08-20 22:38:40 -0300

    fix de staircase.md

--- basic/condicional.js
+++ basic/condicional.js
@@ -7,3 +7,31 @@ console.log('1' == 1);
 console.log('1' === 1);
 
 console.log(null === undefined);
+
+// Valores falsos
+// false, undefined, null, 0, NaN, '' (cadena vacia)
+
+if(a) console.log("a es verdadero");
+
+// while (condition) {
+    
+// }
+
+// do{
+    
+// } while(condicion)
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+    
+// }
+
--- basic/ejercicios/2-staircase.md
+++ basic/ejercicios/2-staircase.md
@@ -1,10 +1,12 @@
 ### Considere una escalera de tamaño n = 6
-     
+
+Formar una escalera con el caracter # por ejemplo la escalera de tamaño 6 va ser:
+
 >
 >       #
 >      ##
 >     ###
 >    ####
 >   #####
->
+>  ######
 
