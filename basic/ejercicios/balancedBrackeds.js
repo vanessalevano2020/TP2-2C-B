a3ecca22c7561677c0a64b4017fdae2d14d1242c
pablofernandezhinojosa <pablof@tecnoshare.com>
2020-08-30 19:22:51 -0300

    desafio balancedBrackeds

---
+++ basic/ejercicios/balancedBrackeds.js
@@ -0,0 +1,11 @@
+// Complete the isBalanced function below.
+function isBalanced(s) {
+
+
+}
+
+// TESTS
+console.log(isBalanced('{[()]}') == 'YES');
+console.log(isBalanced('{[(])}') == 'NO');
+console.log(isBalanced('{{[[(())]]}}') == 'YES');
+console.log(isBalanced('{{[[(())]]}}{}()') == 'YES');
