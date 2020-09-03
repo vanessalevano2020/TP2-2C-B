4ddfaf1a904c2a45599b5dfc83f00e588459c137
pablofernandezhinojosa <pablof@tecnoshare.com>
2020-09-03 19:59:33 -0300

    resolucion ejercicio videos

--- basic/videos.js
+++ basic/videos.js
@@ -25,3 +25,34 @@ const str = `<ul>
 // Tip: convertir a un array de objetos
 // Tip2: split
 
+function getVideos(str){
+  return str
+    .replace('<ul>','')
+    .replace('</ul>','')
+    .split('</li>')
+    .slice(0, -1)
+    .map( video => (
+      {
+        min:  parseInt(video
+                      .split('"')[1]
+                      .split(":")[0]),
+        seg: parseInt(video
+                     .split('"')[1]
+                     .split(":")[1]),
+        tipo: video.split('>')[1]
+      }
+    ));
+}
+
+function getTotalSegundos(videos, tipo){
+  let totalsegundos = 0; 
+    videos
+          .filter( video => video.tipo === tipo)
+          .forEach(video => {
+            totalsegundos += video.min * 60 + video.seg;
+          });
+  return totalsegundos;
+}
+
+
+console.log(getTotalSegundos(getVideos(str), "Redux Video"));
